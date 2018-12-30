3164ecd9a0f55d98515c2a5577d2ae5033dc5488
Sujith Bhat <43529659+mRbinG11@users.noreply.github.com>
2018-12-30 18:28:54 +0530

    30th Dec 2018 06:28pm

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool mSoundPool;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +33,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool=new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
 
         // TODO: Load and get the IDs to identify the sounds
+        mCSoundId=mSoundPool.load(getApplicationContext(),R.raw.note_c,1);
+        mASoundId=mSoundPool.load(getApplicationContext(),R.raw.note_a,1);
+        mBSoundId=mSoundPool.load(getApplicationContext(),R.raw.note_b,1);
+        mGSoundId=mSoundPool.load(getApplicationContext(),R.raw.note_g,1);
+        mFSoundId=mSoundPool.load(getApplicationContext(),R.raw.note_f,1);
+        mESoundId=mSoundPool.load(getApplicationContext(),R.raw.note_e,1);
+        mDSoundId=mSoundPool.load(getApplicationContext(),R.raw.note_d,1);
 
 
     }
 
     // TODO: Add the play methods triggered by the buttons
+    public void playC(View V){
 
+    }
 
+    public void playD(View V){
 
+    }
+
+    public void playE(View V){
+
+    }
+
+    public void playF(View V){
+
+    }
+
+    public void playG(View V){
+
+    }
+
+    public void playA(View V){
+
+    }
+
+    public void playB(View V){
+
+    }
+
+    public
 }
